c7c237c888ee7556c6c17463821d6dd775bc00b6
EverthJPradoB <ejpradob@gmail.com>
2024-07-27 22:18:25 -0500

    app descuento

---
+++ app/src/main/java/com/empresa/app/descuentosapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.empresa.app.descuentosapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.empresa.app.descuentosapp.ui.theme.DescuentosAppTheme
+import com.empresa.app.descuentosapp.views.HomeView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DescuentosAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   HomeView()
+                }
+            }
+        }
+    }
+}
+
+
